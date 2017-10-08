60e59c7c1effe93d2b1a01ee131bfe2010170c6a
Sukrins Shrestha <sshres14@hawk.iit.edu>
2017-10-08 01:26:35 -0500

    Added YouTube API Project

---
+++ shrestha.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+<title>
+REST service implementation by Sukrins Shrestha
+</title>
+</head>
+<body>
+<div class="main-title"><h1>SOA with REST - Lab 2</h1>
+<h2>By: Sukrins Shrestha</h2>
+</div>
+
+<div><br/><h2>Twitter REST services<h2></div>
+    <div class="youtube-container">
+    <?php include 'example.php';?>
+</div>
+</body>
+</html>
